5d43052bfb652a9c3e2a2c21fa8bf658ccc38e2e
Surendra Puri <mr.surendra.puri@gmail.com>
2026-07-21 14:32:11 +0530

    udpated variabless

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 module "aks" {
   source = "git::https://github.com/Surendra-Puri/Terraform-Modules.git//AKS?ref=main"
 
-  aks_name      = "aks-lab"
+  aks_name      = var.aks_name
   location  = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   vm_size   = var.vm_size
